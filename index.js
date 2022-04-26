b845d8cbe2911e9dec17246302711606625224e9
marco junior <marcojuniorengineer@gmail.com>
2022-04-26 08:38:30 -0300

    estrutura

---
+++ index.js
@@ -0,0 +1,30 @@
+import express  from "express"
+import cors from "cors"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.post("/participants", (req, res) () => {
+
+})
+
+app.get("/participants", (req, res) () => {
+    
+})
+
+app.post("/messages", (req, res) () => {
+    
+})
+
+app.get("/messages", (req, res) () => {
+    
+})
+
+app.post("/status", (req, res) () => {
+    
+})
+
+app.listen(5000, () => {
+    console.log("servidor em pé")
+})
