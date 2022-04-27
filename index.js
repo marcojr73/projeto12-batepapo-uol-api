067b246a3834c7256fcc53adfe0137c9a0907b7b
marco junior <marcojuniorengineer@gmail.com>
2022-04-27 10:05:52 -0300

    estrutura no post participants

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
-import express  from "express"
+import express, { text } from "express"
 import cors from "cors"
+import dayjs from "dayjs"
 
 const app = express()
+
+let hour = dayjs().format("HH:mm:ss")
+let date = dayjs().format("DD/MM/YYYY HH:mm:ss")
 app.use(cors())
 app.use(express.json())
 
-app.post("/participants", (req, res) () => {
 
+let participants = []
+let messages = []
+
+app.post("/participants", (req,res) => {
+    let { name } = req.body
+
+    // validação pela biblioteca join a ser inserida aqui
+
+    if(name == ""){
+        res.status(422).send("Todos os campos são obrigatórios!")
+        return
+    }
+    const particampdb = {
+        name: name,
+        lastStatus: Date.now()
+    }
+    const messagedb = {
+        from: "xxx" ,
+        to: "todos" ,
+        text: "entra na sala" ,
+        type: "status" ,
+        time: hour
+    }
+    console.log(mdb)
+    res.status(201)
 })
 
-app.get("/participants", (req, res) () => {
+app.get("/participants", (req, res) => {
     
 })
 
-app.post("/messages", (req, res) () => {
+app.post("/messages", (req, res) => {
     
 })
 
-app.get("/messages", (req, res) () => {
+app.get("/messages", (req, res) => {
     
 })
 
-app.post("/status", (req, res) () => {
+app.post("/status", (req, res) => {
     
 })
 
