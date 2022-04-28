534bbb59b26f7746540caa0360b98a1f334945f7
marco junior <marcojuniorengineer@gmail.com>
2022-04-28 08:59:55 -0300

    conexao db

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import express, { text } from "express"
 import cors from "cors"
 import dayjs from "dayjs"
+import { MongoClient } from "mongodb"
 
 const app = express()
-
-let hour = dayjs().format("HH:mm:ss")
-let date = dayjs().format("DD/MM/YYYY HH:mm:ss")
 app.use(cors())
 app.use(express.json())
 
+let hour = dayjs().format("HH:mm:ss")
+let date = dayjs().format("DD/MM/YYYY HH:mm:ss")
+
+//
+let db = null;
+const mongoClient = new MongoClient("mongodb://localhost:27017") 
+const promisse = mongoClient.connect();
+promisse.then(response => {
+    db = mongoClient.db("test")
+    console.log("banco de dados conectados")
+})
+promisse.catch(e => console.log("deu ruim meu patrão",e))
+//
 
 let participants = []
 let messages = []
@@ -22,11 +33,11 @@ app.post("/participants", (req,res) => {
         res.status(422).send("Todos os campos são obrigatórios!")
         return
     }
-    const particampdb = {
+    const particant = {
         name: name,
         lastStatus: Date.now()
     }
-    const messagedb = {
+    const message = {
         from: "xxx" ,
         to: "todos" ,
         text: "entra na sala" ,
