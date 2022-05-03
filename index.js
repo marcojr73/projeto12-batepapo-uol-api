d2d0e4066c9e59445ad4bf2eff6dde5ec171166c
marco junior <marcojuniorengineer@gmail.com>
2022-05-02 21:34:51 -0300

    mensagens

--- index.js
+++ index.js
@@ -3,28 +3,32 @@ import cors from "cors"
 import dayjs from "dayjs"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
-
+import joi from "joi"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 dotenv.config()
 
-let hour = dayjs().format("HH:mm:ss")
-let date = dayjs().format("DD/MM/YYYY HH:mm:ss")
-
-let db = null;
 const mongoClient = new MongoClient(process.env.URL_MONGO) 
-console.log(process.env.URL_MONGO)
+
+await mongoClient.connect()
+let db = mongoClient.db("test")
+
+
+const participantsSchema = joi.object({
+    name: joi.string().min(1).required()
+})
+const messagesSchema = joi.object({
+    to: joi.string().min(1).required(),
+    text: joi.string().min(1).required(),
+    type: joi.any().valid("message", "private_message").required()
+})
 
 app.post("/participants", async (req,res) => {
     let { name } = req.body
+    let hour = dayjs().format("HH:mm:ss")
     // validação pela biblioteca join a ser inserida aqui
-
-    if(name == ""){
-        res.status(422).send("Todos os campos são obrigatórios!")
-        return
-    }
 
     const participant = {
         name: name,
@@ -40,19 +44,24 @@ app.post("/participants", async (req,res) => {
     }
 
     try{
-        await mongoClient.connect()
-        db = mongoClient.db("test")
-
+        name = await participantsSchema.validateAsync({name})
+        const available = await db.collection("participants").findOne(name)
+        console.log(available, name)
+        if(available){
+            res.send("Usuário já existente")
+            return
+        }
         const status = await db.collection("participants").insertOne(participant)
         await db.collection("messages").insertOne(message)
-
         res.send(status)
-        mongoClient.close()
-
+        
     } catch (e) {
         console.log("deu ruim chefe", e)
+        if(e.isJoi){
+            res.sendStatus(422)
+            return
+        }
         res.send("deu ruim de mais")
-        mongoClient.close()
     }
         
 })
@@ -65,66 +74,75 @@ app.get("/participants", async (req, res) => {
 
         const participants = await db.collection("participants").find({}).toArray()
         res.send(participants)
-        mongoClient.close()
-        return
-        
     } catch (e) {
         console.log("xabuuuuu", e)
         res.send("deu ruim de mais")
-        mongoClient.close()
     }
 
 })
 
 app.post("/messages", async (req, res) => {
-    const { to, text, type } = req.body
-    const user = req.headers.user
-
-    const message = {
-        to: to,
-        from: user,
-        type: type,
-        text: text,
-        time: hour
-    }
+    // const { to, text, type } = req.body
+    const message = req.body
+    const name = req.headers.user
+    let hour = dayjs().format("HH:mm:ss")
 
     try {
-        await mongoClient.connect()
-        db = mongoClient.db("test")
-
+        const result =  await messagesSchema.validateAsync(message)
+        const available = await db.collection("participants").findOne({name: message.to})
+    
+        console.log(available)
+        if(available || message.to == "Todos"){
+            message.from = name
+            message.time = hour
+        } else {
+            res.send("usuário nao esta no servidor")
+            return
+        }
         await db.collection("messages").insertOne(message)
-        res.send("201")
-        mongoClient.close()
+        res.send(message)
     } catch (e) {
-        console.log("deu ruim chefe", e)
+        if(e.isJoi){
+            res.send("tipo de mensagem errada")
+            return
+        }
         res.send("deu ruim de mais")
-        mongoClient.close()
     }
     
 })
 
 app.get("/messages", async (req, res) => {
     const {limit} = req.query
-    const user = req.headers.user
-    console.log(limit, user)
+    const {user} = req.headers
 
     try{
-        // find({$or: [{from:user},{to:user},{to:"todos"}]})
-        
-        await mongoClient.connect()
-        db = mongoClient.db("test")
+        const messages = await db.collection("messages")
+            .find({$or: [{from:user},{to:user},{to:"Todos"}]})
+            .toArray()
 
-        const messages = await db.collection("messages").find({}).toArray()
+        if(limit){
+            res.send(messages.slice(-limit))
+            return
+        }
         res.send(messages)
-        mongoClient.close()
     } catch (e) {
-        console.log("deu ruim", e)
-        mongoClient.close()
+        res.send("erro ao receber as mensagens")
     }
 })
 
-app.post("/status", (req, res) => {
-    
+app.post("/status", async (req, res) => {
+    const {user} = req.headers
+
+    try {
+        await mongoClient.connect()
+        db = mongoClient.db("test")
+
+        await db.collection("participants")
+        .updateOne({user: user}, {$set: {lastStatus: Date.now()}})
+        res.send("deve ter alterado")
+    } catch (e) {
+        console.log("erro", e)
+    }
 })
 
 app.listen(5000, () => {
